1020ac4b339b389237a2385158a5b8abc366de95
konoozgamal <aya476771@gmail.com>
2023-07-29 19:36:29 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
-
+import { BrowserRouter , Routes, Route } from "react-router-dom";
+import Header from './Components/dashboard/Layout/Header';
+import Home from './Pages/Home/Home';
+import Services from './Pages/Services/Services';
+import Reviews from './Pages/Reviews/Reviews';
+import Footer from './Components/dashboard/Layout/Footer/Footer'
 function App() {
   return (
-    <div className="App">
-     <h1>Hello world</h1>
-    </div>
+    <>
+       <BrowserRouter>
+        <Header/>
+        <Routes>
+           {/* ==================== Header Navlink Start here =================== */}
+          <Route path="/" element={<Home />} />
+          <Route path="/services"  element={<Services />} />
+          <Route path="/reviews"  element={<Reviews />} />
+           {/* ==================== Header Navlink End here =================== */}
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/Components/dashboard/Layout/Header.js
+++ src/Components/dashboard/Layout/Header.js
@@ -1,7 +1,31 @@
-import React from 'react'
-
+import React from 'react';
+import './Header.module.css';
+import {NavLink} from 'react-router-dom';
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 export default function Header() {
+  const navBarStyles =({isActive})=>{
+    return{
+     textDecoration: isActive ?'underline':'none',
+     color:isActive ? '#A201FF':'#000000' ,
+    }
+  }
   return (
-    <div>Header</div>
+    <header>
+      <div className='logo'>
+       <img src='' alt='logo' />
+      </div>
+      <nav>
+       <NavLink style={navBarStyles} to="/">Home</NavLink>
+       <NavLink style={navBarStyles} to="/services">Services</NavLink>
+       <NavLink style={navBarStyles} to="/reviews">Reviews</NavLink>
+      </nav>
+      <div className='Icons'>
+        <div><SearchIcon/></div>
+        <div><LanguageIcon/></div>
+        <div><AccountCircleIcon/></div>
+      </div>
+    </header>
   )
 }
